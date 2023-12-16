b357992c8f5b615df83d6536abfd0ca61dff666e
Legendleo90 <akshatleo9456@gmail.com>
2023-12-16 08:25:10 +0530

    [SQUASH]sdm845-common: parts: dirac: Drop Hi-Fi and MiSound scenes
    This reverts commit 18dd6250cad915ada8e7bed220c368b507cae9c9.
    
    Revert "sdm845-common: parts: Enable hi-fi mode regardless headset and preset"
    
    This reverts commit 5b87276228fb8cdb0b5b52b1a036b540b373e8ca.
    
    Revert "sdm845-common: parts: Add MiSound scenes"
    
    This reverts commit 2cc0197b9651ab03ce7ca80818cd43e42f2e58fa.
    
    Revert "sdm845-common: parts: Disable Hi-Fi preference when toggling switchbar"
    
    This reverts commit 4d182965b477da54f2a98b9ba0d2814cdde139f6.
    
    Revert "sdm845-common: parts: Use headphones icon for Dirac QS tile"
    
    This reverts commit 90e827e1b04eb59c678c326bef5c784760f50833.
    
    Revert "sdm845-common: parts: Refresh playback if necessary"
    
    This reverts commit feb5894ef063c46fd548b98183efc40947f9f64c.
    
    Revert "sdm845-common: parts: Add Dirac QS tile"
    
    This reverts commit 7a3a0de5eba045f79215be90677857d97737c4a0.
    
    Revert "sdm845-common: parts: Use proper singleton pattern for DiracUtils"
    
    This reverts commit a831cc04a0e877eb09015c9fc1c98b8d19fe50f1.
    
    Revert "sdm845-common: parts: Handle more error for dirac"
    
    This reverts commit 5daf88e80e9434f8c792f5aadb4202bb9640e278.
    
    Revert "sdm845-common: parts: Dirac fixes"
    
    This reverts commit 28c9c7116ba56ded1bc5245f8d67ee2496fc4c0c.
    
    Revert "sdm845-common: Add support for hifi"
    
    This reverts commit 80c621c8de4da33a3cecd506e98e568e647a31aa.
    
    Revert "sdm845-common: parts: Update Dirac Tile label"
    
    This reverts commit 02f902f0e6701a85e2aaf92de5e6933d7441183b.
    
    Revert "sdm845-common: parts: Add activity for handling QS tile long click action"
    
    This reverts commit d2228713b6cc3bbc47962b7353313d4ac56a0200.

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -18,7 +18,6 @@ package org.lineageos.settings.dirac;
 
 import android.os.Bundle;
 import android.widget.Switch;
-import android.util.Log;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -34,69 +33,43 @@ import org.lineageos.settings.R;
 public class DiracSettingsFragment extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
 
-    private static final String TAG = "DiracSettingsFragment";
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
-    private static final String PREF_HIFI = "dirac_hifi_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
-    private static final String PREF_SCENE = "scenario_selection";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
-    private ListPreference mScenes;
-    private SwitchPreference mHifi;
-    private DiracUtils mDiracUtils;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.dirac_settings);
 
-        try {
-            mDiracUtils = DiracUtils.getInstance(getActivity());
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
+        DiracUtils.initialize();
+        boolean enhancerEnabled = DiracUtils.isDiracEnabled(getActivity());
 
-        boolean enhancerEnabled = mDiracUtils != null ? mDiracUtils.isDiracEnabled() : false;
         mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
         mSwitchBar.addOnSwitchChangeListener(this);
         mSwitchBar.setChecked(enhancerEnabled);
 
         mHeadsetType = (ListPreference) findPreference(PREF_HEADSET);
         mHeadsetType.setOnPreferenceChangeListener(this);
+        mHeadsetType.setEnabled(enhancerEnabled);
 
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
-
-        mHifi = (SwitchPreference) findPreference(PREF_HIFI);
-        mHifi.setOnPreferenceChangeListener(this);
-
-        mScenes = (ListPreference) findPreference(PREF_SCENE);
-        mScenes.setOnPreferenceChangeListener(this);
-
-        mHeadsetType.setEnabled(enhancerEnabled);
         mPreset.setEnabled(enhancerEnabled);
-        mHifi.setEnabled(enhancerEnabled);
-        mScenes.setEnabled(enhancerEnabled);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (mDiracUtils == null) return false;
         switch (preference.getKey()) {
             case PREF_HEADSET:
-                mDiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
-                return true;
-            case PREF_HIFI:
-                mDiracUtils.setHifiMode((Boolean) newValue ? 1 : 0);
+                DiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
                 return true;
             case PREF_PRESET:
-               mDiracUtils.setLevel(String.valueOf(newValue));
-                return true;
-            case PREF_SCENE:
-                mDiracUtils.setScenario(Integer.parseInt(newValue.toString()));
+                DiracUtils.setLevel(String.valueOf(newValue));
                 return true;
             default:
                 return false;
@@ -107,11 +80,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     public void onSwitchChanged(Switch switchView, boolean isChecked) {
         mSwitchBar.setChecked(isChecked);
 
-        if (mDiracUtils == null) return;
-        mDiracUtils.setEnabled(isChecked);
-        mHifi.setEnabled(isChecked);
+        DiracUtils.setMusic(isChecked);
+
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
-        mScenes.setEnabled(isChecked);
     }
 }
