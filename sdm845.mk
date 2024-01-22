1122c8b5b5b99f0a89ca09b30b7e90e450c33395
PSKMaxi <123083334+PSKMaxi@users.noreply.github.com>
2024-01-22 21:41:00 +0530

    Update sdm845.mk

--- sdm845.mk
+++ sdm845.mk
@@ -264,6 +264,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm845
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
